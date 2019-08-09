5844210d87d8140587abe094727994a459df1a99
Amir Bar <havainc@gmail.com>
2019-08-08 19:15:09 -0700

    merge

--- models/burgers.js
+++ models/burgers.js
@@ -3,18 +3,18 @@ var orm = require('../config/orm.js');
 
 var burger = {
 	all: function (cb) {
-		orm.all('Burgers', function (res) {
+		orm.all('burgers', function (res) {
 			cb(res);
 		});
 	},
 	update: function (id, cb) {
-		orm.update('Burgers',id, cb, function (res) {
+		orm.update('burgers',id, cb, function (res) {
 			cb(res);
 		});
 	},
 
 	create: function (name, cb) {
-		orm.create('Burgers',name, cb); 
+		orm.create('burgers',name, cb); 
 	}
 }
 
--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.use(express.static(process.cwd() + '/public'));
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
-app.get("/", function(req, res) {
-    res.json(path.join(__dirname, "public/index.html"));
-  });
+
 app.use(methodOverride('_method'));
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
